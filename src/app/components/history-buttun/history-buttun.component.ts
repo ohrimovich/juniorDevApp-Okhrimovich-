3b82f3471b716c17b31882faf4e10b5c88ffdcbf
ohrimovich <ohrimovich.sanek@gmailcom>
2020-08-27 13:01:57 +0300

    add template for modal window

--- src/app/components/history-buttun/history-buttun.component.ts
+++ src/app/components/history-buttun/history-buttun.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { RandomNumberService } from 'src/app/services/random-number.service';
+import { MatDialog } from '@angular/material/dialog';
 
 
 @Component({
@@ -7,11 +8,20 @@ import { RandomNumberService } from 'src/app/services/random-number.service';
   templateUrl: './history-buttun.component.html',
   styleUrls: ['./history-buttun.component.scss']
 })
-export class HistoryButtunComponent implements OnInit {
+export class HistoryButtunComponent {
 
-  constructor(public randomNumberService: RandomNumberService) { }
+  constructor(public dialog: MatDialog) { }
 
-  ngOnInit(): void {
-  }
+  openDialog() {
+    const dialogRef = this.dialog.open(DialogContent);
 
 }
+}
+
+    @Component({
+      selector: 'dialog-content',
+      templateUrl: './dialog-content.html',
+    })
+    export class DialogContent {
+      constructor(public randomNumberService: RandomNumberService) {}
+    }
