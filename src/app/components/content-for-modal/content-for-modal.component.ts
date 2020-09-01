0349f8f03bed8b90862bbf7c0fd8f937866c86fb
ohrimovich <ohrimovich.sanek@gmailcom>
2020-09-01 22:44:53 +0300

    content for modal

---
+++ src/app/components/content-for-modal/content-for-modal.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+@Component({
+  selector: 'app-content-for-modal',
+  templateUrl: './content-for-modal.component.html',
+  styleUrls: ['./content-for-modal.component.scss']
+})
+export class ContentForModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+
+}
